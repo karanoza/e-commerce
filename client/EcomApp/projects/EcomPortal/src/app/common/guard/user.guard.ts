c5140fe878ca4df8770eb52164f51f83bba15779
Karan <karanoza94@gmail.com>
2020-09-20 19:09:24 +0530

    fixes

--- client/EcomApp/projects/EcomPortal/src/app/common/guard/user.guard.ts
+++ client/EcomApp/projects/EcomPortal/src/app/common/guard/user.guard.ts
@@ -43,37 +43,4 @@ export class UserGuard implements CanActivate {
       return false;
     }
   }
-
-  // constructor(private encService: EncDecService, private router: Router) {}
-
-  // canActivate(
-  //   next: ActivatedRouteSnapshot,
-  //   state: RouterStateSnapshot
-  // ):
-  //   | Observable<boolean | UrlTree>
-  //   | Promise<boolean | UrlTree>
-  //   | boolean
-  //   | UrlTree {
-  //   if (sessionStorage.getItem("role") != null) {
-  //     const role = this.encService.decrypt(sessionStorage.getItem("role"), "");
-  //     if (role === "User") {
-  //       return true;
-  //     } else {
-  //       this.navigateToLogin(state);
-  //     }
-  //   } else {
-  //     this.navigateToLogin(state);
-  //   }
-  // }
-  // canLoad(
-  //   route: Route,
-  //   segments: UrlSegment[]
-  // ): Observable<boolean> | Promise<boolean> | boolean {
-  //   return true;
-  // }
-
-  // navigateToLogin(state: RouterStateSnapshot) {
-  //   this.router.navigate(["/login"], { queryParams: { returnUrl: state.url } });
-  //   return false;
-  // }
 }
--- client/EcomApp/projects/EcomPortal/src/app/user/guard/user.guard.ts
+++ client/EcomApp/projects/EcomPortal/src/app/user/guard/user.guard.ts
@@ -16,24 +16,6 @@ import { Observable } from "rxjs";
   providedIn: "root",
 })
 export class UserGuard implements CanLoad {
-  // constructor(private encService: EncDecService, private router: Router) {}
-
-  // canLoad(
-  //   route: Route,
-  //   segments: UrlSegment[]
-  // ): Observable<boolean> | Promise<boolean> | boolean {
-  //   if (sessionStorage.getItem("role") != null) {
-  //     const role = this.encService.decrypt(sessionStorage.getItem("role"), "");
-  //     if (role === "User") {
-  //       return true;
-  //     } else {
-  //       return false;
-  //     }
-  //   } else {
-  //     return false;
-  //   }
-  // }
-
   constructor(private encService: EncDecService, private router: Router) {}
 
   canActivate(
